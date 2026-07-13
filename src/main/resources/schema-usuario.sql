b7e3e998539f4f7da07e80e3dd9740a7171132e7
ferclager <17533449+ferclager@users.noreply.github.com>
2026-07-13 11:45:06 +0200

    add schema-usuario - new table

---
+++ src/main/resources/schema-usuario.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS usuario (
+    id     BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email  VARCHAR(150) NOT NULL,
+    edad   INT          NOT NULL
+);

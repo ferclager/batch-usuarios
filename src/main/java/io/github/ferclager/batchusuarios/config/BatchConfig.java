e59e2d7e93f54e9d74a271180e5eb028186f3eb1
ferclager <17533449+ferclager@users.noreply.github.com>
2026-07-16 12:47:38 +0200

    add jobParameters to the reader

--- src/main/java/io/github/ferclager/batchusuarios/config/BatchConfig.java
+++ src/main/java/io/github/ferclager/batchusuarios/config/BatchConfig.java
@@ -4,18 +4,20 @@ import io.github.ferclager.batchusuarios.model.Usuario;
 import io.github.ferclager.batchusuarios.processor.UsuarioProcessor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.RecordFieldSetMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
@@ -24,10 +26,11 @@ import javax.sql.DataSource;
 public class BatchConfig {
 
     @Bean
-    public ItemReader<Usuario> reader() {
+    @StepScope
+    public FlatFileItemReader<Usuario> reader(@Value("#{jobParameters['rutaFichero']}") String fichero) {
         return new FlatFileItemReaderBuilder<Usuario>()
                 .name("usuarioReader")
-                .resource(new ClassPathResource("data/usuarios.csv"))
+                .resource(new FileSystemResource(fichero))
                 .linesToSkip(1)
                 .delimited().names("nombre", "email", "edad")
                 .fieldSetMapper(new RecordFieldSetMapper<>(Usuario.class))
@@ -46,7 +49,7 @@ public class BatchConfig {
     @Bean
     public Step pasoImportar(JobRepository jobRepository,
                              PlatformTransactionManager tx,
-                             ItemReader<Usuario> reader,
+                             FlatFileItemReader<Usuario> reader,
                              UsuarioProcessor processor,
                              ItemWriter<Usuario> writer) {
         return new StepBuilder("pasoImportar", jobRepository)
